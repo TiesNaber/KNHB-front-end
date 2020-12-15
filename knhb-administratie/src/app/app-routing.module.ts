49bbf88be73a0f9f11ee979c6bf14d9bd14e0a73
Ties Naber <tiesnaber@live.nl>
2020-12-15 11:06:42 +0100

    Added routing to team

--- knhb-administratie/src/app/app-routing.module.ts
+++ knhb-administratie/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {ClubsComponent } from './clubs/clubs.component';
 import {ClubDetailComponent} from './club-detail/club-detail.component';
-
+import {TeamsComponent} from './teams/teams.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/clubs', pathMatch: 'full' },  
   {path: 'clubs', component: ClubsComponent},
-  {path: 'clubs/:club_ID', component: ClubDetailComponent}
+  {path: 'clubs/:club_ID', component: ClubDetailComponent},
+  {path: 'teams/:team_ID', component: TeamsComponent}
 
 ];
 
